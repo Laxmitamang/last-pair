6ada3e220f1b75421aa085f7ada49bb5695569c4
Laxmitamang <51143177+Laxmitamang@users.noreply.github.com>
2026-08-20 09:38:48 +0100

    test: introduce intentional CI lint failure

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,6 @@ import { Storefront } from "./storefront";
 import { products } from "./products";
 
 export default function Home() {
+  const intentionalCiFailure: any = "This unsafe type is only for our CI exercise";
   return <Storefront products={products} />;
 }
